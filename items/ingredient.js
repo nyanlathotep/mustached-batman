e6b4495fe331fb5a8cf7f471ea120c7ff976c8e4
pseudobyte <pseudobyte@gmail.com>
2015-03-19 23:26:35 -0600

    initial commit

---
+++ items/ingredient.js
@@ -0,0 +1,67 @@
+name = "ingredient";
+creativeTab = "Medicine";
+maxStack = 64;
+
+addToCreative[0] = true;
+textureFile[0] = "powder_herbal.png";
+information[0] = "A pile of green goop.|This is the base of some|simple potions."
+
+addToCreative[1] = true;
+textureFile[1] = "powder_fungal.png";
+information[1] = "A pile of grey goop.|This is the base of some|simple potions."
+
+addToCreative[2] = true;
+textureFile[2] = "powder_root.png";
+information[2] = "A pile of brown goop.|This is the base of some|simple potions."
+
+addToCreative[3] = true;
+textureFile[3] = "powder_cactus.png";
+information[3] = "A pile of rich green goop.|This is the base of some|simple potions."
+
+addToCreative[4] = true;
+textureFile[4] = "bowl_red";
+information[4] = "Your eyes are drawn to it."
+
+addToCreative[5] = true;
+textureFile[5] = "bowl_orange";
+information[5] = "It practically jumps out|at you."
+
+addToCreative[6] = true;
+textureFile[6] = "bowl_pink";
+information[6] = "You have difficulty focusing|on it."
+
+addToCreative[7] = true;
+textureFile[7] = "bowl_purple";
+information[7] = "It has a deep healthy look|to it."
+
+addToCreative[8] = true;
+textureFile[8] = "bowl_magenta";
+information[8] = "It left a rash on your hand."
+
+addToCreative[9] = true;
+textureFile[9] = "bowl_yellow";
+information[9] = "It seems heavier than a|moment ago."
+
+addToCreative[10] = true;
+textureFile[10] = "bowl_white";
+information[10] = "Seems vicious."
+
+addToCreative[11] = true;
+textureFile[11] = "bowl_ltblue";
+information[11] = "It seems to move about|excitedly."
+
+addToCreative[12] = true;
+textureFile[12] = "bowl_brown";
+information[12] = "It is comfortably warm|to the touch."
+
+addToCreative[13] = true;
+textureFile[13] = "bowl_ltgrey";
+information[13] = "It keeps springing back|against your touch."
+
+addToCreative[14] = true;
+textureFile[14] = "bowl_blue";
+information[14] = "It is extremely watery."
+
+addToCreative[15] = true;
+textureFile[15] = "powder_watery";
+information[15] = "A pile of bluegreen goop.|It might work in a potion."
---
+++ items/potion.js
@@ -0,0 +1,67 @@
+
+
+addToCreative[4] = true;
+textureFile[4] = "vial_red";
+alwaysEdible[4] = true;
+usingAction[4] = "drink";
+onEat[4] = "if (Math.random() < 0.2) {player.addPotionEffect('blindness', 150, 0);} else {player.addPotionEffect('nightVision', 900, 0);}";
+
+addToCreative[5] = true;
+textureFile[5] = "vial_orange";
+alwaysEdible[5] = true;
+usingAction[5] = "drink";
+onEat[5] = "if (Math.random() < 0.2) {player.addPotionEffect('slowness', 150, 0);} else {player.addPotionEffect('jump', 900, 0);}";
+
+addToCreative[6] = true;
+textureFile[6] = "vial_pink";
+alwaysEdible[6] = true;
+usingAction[6] = "drink";
+onEat[6] = "if (Math.random() < 0.2) {player.addPotionEffect('blindness', 150, 0);} else {player.addPotionEffect('invisibility', 900, 0);}";
+
+addToCreative[7] = true;
+textureFile[7] = "vial_purple";
+alwaysEdible[7] = true;
+usingAction[7] = "drink";
+onEat[7] = "if (Math.random() < 0.2) {player.addPotionEffect('nausea', 150, 0)} else {player.addPotionEffect('poison', 0, 255);}";
+
+addToCreative[8] = true;
+textureFile[8] = "vial_magenta";
+alwaysEdible[8] = true;
+usingAction[8] = "drink";
+onEat[8] = "if (Math.random() < 0.2) {player.addPotionEffect('digSlowDown', 150, 0);} else {player.addPotionEffect('digspeed', 900, 0);}";
+
+addToCreative[9] = true;
+textureFile[9] = "vial_yellow";
+alwaysEdible[9] = true;
+usingAction[9] = "drink";
+onEat[9] = "if (Math.random() < 0.2) {player.addPotionEffect('poison', 100, 0);} else {player.addPotionEffect('absorption', 900, 0);}";
+
+addToCreative[10] = true;
+textureFile[10] = "vial_white";
+alwaysEdible[10] = true;
+usingAction[10] = "drink";
+onEat[10] = "if (Math.random() < 0.2) {player.addPotionEffect('weakness', 100, 0);} else {player.addPotionEffect('damageBoost', 900, 0);}";
+
+addToCreative[11] = true;
+textureFile[11] = "vial_ltblue";
+alwaysEdible[11] = true;
+usingAction[11] = "drink";
+onEat[11] = "if (Math.random() < 0.2) {player.addPotionEffect('moveSlowdown', 100, 0);} else {player.addPotionEffect('moveSpeed', 900, 0);}";
+
+addToCreative[12] = true;
+textureFile[12] = "vial_brown";
+alwaysEdible[12] = true;
+usingAction[12] = "drink";
+onEat[12] = "if (Math.random() < 0.2) {player.setFire(3);} else {player.addPotionEffect('fireResistance', 900, 0);}";
+
+addToCreative[13] = true;
+textureFile[13] = "vial_ltgrey";
+alwaysEdible[13] = true;
+usingAction[13] = "drink";
+onEat[13] = "if (Math.random() < 0.2) {player.addPotionEffect('harm', 0, 0);} else {player.addPotionEffect('resistance', 900, 0);}";
+
+addToCreative[14] = true;
+textureFile[14] = "vial_blue";
+alwaysEdible[14] = true;
+usingAction[14] = "drink";
+onEat[14] = "if (Math.random() < 0.2) {player.addPotionEffect('harm', 0, 0);} else {player.addPotionEffect('waterBreathing', 900, 0);}";
