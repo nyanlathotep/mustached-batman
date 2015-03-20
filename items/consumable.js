1f1dab4a44b9383936ddf38543b77a8d6018097c
pseudobyte <pseudobyte@gmail.com>
2015-03-20 02:36:15 -0600

    added consumable items

---
+++ items/consumable.js
@@ -0,0 +1,13 @@
+name = "consumable";
+creativeTab = "Medicine";
+maxStack = 8;
+
+addToCreative[0] = true;
+textureFile[0] = "meds_poultice";
+information[0] = "A makeshift bandage.|Sneak-right click to apply.";
+onRightClick[0] = "if (player.isSneaking()) {if (player.getMaxHealth() - player.getHealth() > 0) {itemstack.setStackSize(itemstack.getStackSize() - 1); player.setHealth(player.getHealth() + 1);} else {player.sendMessage('You\\'re in good shape, no need to patch up.');}}";
+
+addToCreative[1] = true;
+textureFile[1] = "meds_bandage";
+information[1] = "A treated adhesive bandage.|Sneak-right click to apply.";
+onRightClick[1] = "if (player.isSneaking()) {if (player.getMaxHealth() - player.getHealth() > 0) {itemstack.setStackSize(itemstack.getStackSize() - 1); player.setHealth(player.getHealth() + 3);} else {player.sendMessage('You\\'re in good shape, no need to patch up.');}}";
