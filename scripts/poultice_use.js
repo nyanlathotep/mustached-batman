55703c9a9fc6b071e3f47abc0cda2b35033bfc39
pseudobyte <pseudobyte@gmail.com>
2015-03-20 20:48:16 -0600

    made bandages and poultices cap at player max health

--- scripts/poultice_use.js
+++ scripts/poultice_use.js
@@ -1,7 +1,7 @@
 if (player.isSneaking()) {
   if (player.getMaxHealth() == player.getHealth()) {
     itemstack.setStackSize(itemstack.getStackSize() - 1);
-    player.setHealth(player.getHealth() + 1);
+    player.setHealth(Math.min(player.getHealth() + 1, player.getMaxHealth()));
   } else {
     player.sendMessage('You\'re in good shape, no need to patch up.');
   }
