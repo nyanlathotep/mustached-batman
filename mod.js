764ba5ebea4c65cd90a3151e26336a38da2c0f4b
pseudobyte <pseudobyte@gmail.com>
2015-03-21 15:43:14 -0600

    removed folder for gen's output, pointed it at correct places, added generated files, configured mod.js to use them

--- mod.js
+++ mod.js
@@ -1,11 +1,17 @@
+// instantiate items
 mod.addItem("ingredient.js", "normal");
+mod.addItem("tincture.js", "food");
 mod.addItem("potion.js", "food");
+mod.addItem("elixir.js", "food");
 mod.addItem("consumable.js", "normal");
 
+// general config/utility stuff
 mod.addCreativeTab("Medicine", "addedhealthmechanics:ingredient:0");
-
 mod.loadScript("config.js")
 mod.loadScript("alias.js")
+
+// set up recipes
+mod.loadScript("alchemy.js")
 mod.loadScript("extract.js")
 mod.loadScript("reagent.js")
 mod.loadScript("medicine.js")
---
+++ scripts/alchemy.js
@@ -0,0 +1,218 @@
+mod.addToOreDictionary("AHM_tincture_nightVision", "addedhealthmechanics:tincture:1");
+mod.addToOreDictionary("AHM_tincture_nightVision", "addedhealthmechanics:tincture:0");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:0', 'addedhealthmechanics:ingredient:4', 'fungalGoo', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:1', ':AHM_tincture_nightVision', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_jump", "addedhealthmechanics:tincture:4");
+mod.addToOreDictionary("AHM_tincture_jump", "addedhealthmechanics:tincture:3");
+mod.addToOreDictionary("AHM_tincture_jump", "addedhealthmechanics:tincture:2");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:2', 'addedhealthmechanics:ingredient:5', 'rootPowder', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:4', ':AHM_tincture_jump', 'upgStrengthBase', 'harvestcraft:chilipepperItem', 'harvestcraft:spiceleafItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:3', ':AHM_tincture_jump', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_invisibility", "addedhealthmechanics:tincture:6");
+mod.addToOreDictionary("AHM_tincture_invisibility", "addedhealthmechanics:tincture:5");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:5', 'addedhealthmechanics:ingredient:6', 'fungalGoo', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:6', ':AHM_tincture_invisibility', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_digSpeed", "addedhealthmechanics:tincture:9");
+mod.addToOreDictionary("AHM_tincture_digSpeed", "addedhealthmechanics:tincture:8");
+mod.addToOreDictionary("AHM_tincture_digSpeed", "addedhealthmechanics:tincture:7");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:7', 'addedhealthmechanics:ingredient:7', 'fungalGoo', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:9', ':AHM_tincture_digSpeed', 'upgStrengthBase', 'harvestcraft:chilipepperItem', 'harvestcraft:spiceleafItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:8', ':AHM_tincture_digSpeed', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_absorption", "addedhealthmechanics:tincture:12");
+mod.addToOreDictionary("AHM_tincture_absorption", "addedhealthmechanics:tincture:11");
+mod.addToOreDictionary("AHM_tincture_absorption", "addedhealthmechanics:tincture:10");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:10', 'addedhealthmechanics:ingredient:8', 'herbalPaste', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:12', ':AHM_tincture_absorption', 'upgStrengthBase', 'harvestcraft:chilipepperItem', 'harvestcraft:spiceleafItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:11', ':AHM_tincture_absorption', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_damageBoost", "addedhealthmechanics:tincture:15");
+mod.addToOreDictionary("AHM_tincture_damageBoost", "addedhealthmechanics:tincture:14");
+mod.addToOreDictionary("AHM_tincture_damageBoost", "addedhealthmechanics:tincture:13");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:13', 'addedhealthmechanics:ingredient:9', 'cactusGel', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:15', ':AHM_tincture_damageBoost', 'upgStrengthBase', 'harvestcraft:chilipepperItem', 'harvestcraft:spiceleafItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:14', ':AHM_tincture_damageBoost', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_moveSpeed", "addedhealthmechanics:tincture:18");
+mod.addToOreDictionary("AHM_tincture_moveSpeed", "addedhealthmechanics:tincture:17");
+mod.addToOreDictionary("AHM_tincture_moveSpeed", "addedhealthmechanics:tincture:16");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:16', 'addedhealthmechanics:ingredient:10', 'rootPowder', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:18', ':AHM_tincture_moveSpeed', 'upgStrengthBase', 'harvestcraft:chilipepperItem', 'harvestcraft:spiceleafItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:17', ':AHM_tincture_moveSpeed', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_fireResistance", "addedhealthmechanics:tincture:20");
+mod.addToOreDictionary("AHM_tincture_fireResistance", "addedhealthmechanics:tincture:19");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:19', 'addedhealthmechanics:ingredient:11', 'cactusGel', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:20', ':AHM_tincture_fireResistance', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_resistance", "addedhealthmechanics:tincture:23");
+mod.addToOreDictionary("AHM_tincture_resistance", "addedhealthmechanics:tincture:22");
+mod.addToOreDictionary("AHM_tincture_resistance", "addedhealthmechanics:tincture:21");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:21', 'addedhealthmechanics:ingredient:12', 'herbalPaste', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:23', ':AHM_tincture_resistance', 'upgStrengthBase', 'harvestcraft:chilipepperItem', 'harvestcraft:spiceleafItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:22', ':AHM_tincture_resistance', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_tincture_waterBreathing", "addedhealthmechanics:tincture:25");
+mod.addToOreDictionary("AHM_tincture_waterBreathing", "addedhealthmechanics:tincture:24");
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:24', 'addedhealthmechanics:ingredient:13', 'aqueousPulp', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:tincture:25', ':AHM_tincture_waterBreathing', 'upgLengthBase', 'minecraft:sugar', 'harvestcraft:groundnutmegItem');
+
+mod.addToOreDictionary("AHM_potion_nightVision", "addedhealthmechanics:potion:1");
+mod.addToOreDictionary("AHM_potion_nightVision", "addedhealthmechanics:potion:0");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:1', ':AHM_potion_nightVision', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:0', ':AHM_tincture_nightVision', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_jump", "addedhealthmechanics:potion:4");
+mod.addToOreDictionary("AHM_potion_jump", "addedhealthmechanics:potion:3");
+mod.addToOreDictionary("AHM_potion_jump", "addedhealthmechanics:potion:2");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:4', ':AHM_potion_jump', 'upgStrengthBase', 'ThermalFoundation:material:35', 'minecraft:rotten_flesh');
+mod.addShapelessRecipe('addedhealthmechanics:potion:3', ':AHM_potion_jump', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:2', ':AHM_tincture_jump', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_invisibility", "addedhealthmechanics:potion:6");
+mod.addToOreDictionary("AHM_potion_invisibility", "addedhealthmechanics:potion:5");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:6', ':AHM_potion_invisibility', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:5', ':AHM_tincture_invisibility', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_digSpeed", "addedhealthmechanics:potion:9");
+mod.addToOreDictionary("AHM_potion_digSpeed", "addedhealthmechanics:potion:8");
+mod.addToOreDictionary("AHM_potion_digSpeed", "addedhealthmechanics:potion:7");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:9', ':AHM_potion_digSpeed', 'upgStrengthBase', 'ThermalFoundation:material:35', 'minecraft:rotten_flesh');
+mod.addShapelessRecipe('addedhealthmechanics:potion:8', ':AHM_potion_digSpeed', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:7', ':AHM_tincture_digSpeed', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_absorption", "addedhealthmechanics:potion:12");
+mod.addToOreDictionary("AHM_potion_absorption", "addedhealthmechanics:potion:11");
+mod.addToOreDictionary("AHM_potion_absorption", "addedhealthmechanics:potion:10");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:12', ':AHM_potion_absorption', 'upgStrengthBase', 'ThermalFoundation:material:35', 'minecraft:rotten_flesh');
+mod.addShapelessRecipe('addedhealthmechanics:potion:11', ':AHM_potion_absorption', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:10', ':AHM_tincture_absorption', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_damageBoost", "addedhealthmechanics:potion:15");
+mod.addToOreDictionary("AHM_potion_damageBoost", "addedhealthmechanics:potion:14");
+mod.addToOreDictionary("AHM_potion_damageBoost", "addedhealthmechanics:potion:13");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:15', ':AHM_potion_damageBoost', 'upgStrengthBase', 'ThermalFoundation:material:35', 'minecraft:rotten_flesh');
+mod.addShapelessRecipe('addedhealthmechanics:potion:14', ':AHM_potion_damageBoost', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:13', ':AHM_tincture_damageBoost', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_moveSpeed", "addedhealthmechanics:potion:18");
+mod.addToOreDictionary("AHM_potion_moveSpeed", "addedhealthmechanics:potion:17");
+mod.addToOreDictionary("AHM_potion_moveSpeed", "addedhealthmechanics:potion:16");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:18', ':AHM_potion_moveSpeed', 'upgStrengthBase', 'ThermalFoundation:material:35', 'minecraft:rotten_flesh');
+mod.addShapelessRecipe('addedhealthmechanics:potion:17', ':AHM_potion_moveSpeed', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:16', ':AHM_tincture_moveSpeed', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_fireResistance", "addedhealthmechanics:potion:20");
+mod.addToOreDictionary("AHM_potion_fireResistance", "addedhealthmechanics:potion:19");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:20', ':AHM_potion_fireResistance', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:19', ':AHM_tincture_fireResistance', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_resistance", "addedhealthmechanics:potion:23");
+mod.addToOreDictionary("AHM_potion_resistance", "addedhealthmechanics:potion:22");
+mod.addToOreDictionary("AHM_potion_resistance", "addedhealthmechanics:potion:21");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:23', ':AHM_potion_resistance', 'upgStrengthBase', 'ThermalFoundation:material:35', 'minecraft:rotten_flesh');
+mod.addShapelessRecipe('addedhealthmechanics:potion:22', ':AHM_potion_resistance', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:21', ':AHM_tincture_resistance', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_potion_waterBreathing", "addedhealthmechanics:potion:25");
+mod.addToOreDictionary("AHM_potion_waterBreathing", "addedhealthmechanics:potion:24");
+
+mod.addShapelessRecipe('addedhealthmechanics:potion:25', ':AHM_potion_waterBreathing', 'upgLengthBase', 'ThermalFoundation:material:34', 'minecraft:spider_eye');
+mod.addShapelessRecipe('addedhealthmechanics:potion:24', ':AHM_tincture_waterBreathing', 'upgTierBase', 'harvestcraft:honeyItem', 'minecraft:dye:4');
+
+mod.addToOreDictionary("AHM_elixir_nightVision", "addedhealthmechanics:elixir:1");
+mod.addToOreDictionary("AHM_elixir_nightVision", "addedhealthmechanics:elixir:0");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:1', ':AHM_elixir_nightVision', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:0', ':AHM_potion_nightVision', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_jump", "addedhealthmechanics:elixir:4");
+mod.addToOreDictionary("AHM_elixir_jump", "addedhealthmechanics:elixir:3");
+mod.addToOreDictionary("AHM_elixir_jump", "addedhealthmechanics:elixir:2");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:4', ':AHM_elixir_jump', 'upgStrengthBase', 'minecraft:blaze_powder', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:3', ':AHM_elixir_jump', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:2', ':AHM_potion_jump', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_invisibility", "addedhealthmechanics:elixir:6");
+mod.addToOreDictionary("AHM_elixir_invisibility", "addedhealthmechanics:elixir:5");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:6', ':AHM_elixir_invisibility', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:5', ':AHM_potion_invisibility', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_digSpeed", "addedhealthmechanics:elixir:9");
+mod.addToOreDictionary("AHM_elixir_digSpeed", "addedhealthmechanics:elixir:8");
+mod.addToOreDictionary("AHM_elixir_digSpeed", "addedhealthmechanics:elixir:7");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:9', ':AHM_elixir_digSpeed', 'upgStrengthBase', 'minecraft:blaze_powder', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:8', ':AHM_elixir_digSpeed', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:7', ':AHM_potion_digSpeed', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_absorption", "addedhealthmechanics:elixir:12");
+mod.addToOreDictionary("AHM_elixir_absorption", "addedhealthmechanics:elixir:11");
+mod.addToOreDictionary("AHM_elixir_absorption", "addedhealthmechanics:elixir:10");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:12', ':AHM_elixir_absorption', 'upgStrengthBase', 'minecraft:blaze_powder', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:11', ':AHM_elixir_absorption', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:10', ':AHM_potion_absorption', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_damageBoost", "addedhealthmechanics:elixir:15");
+mod.addToOreDictionary("AHM_elixir_damageBoost", "addedhealthmechanics:elixir:14");
+mod.addToOreDictionary("AHM_elixir_damageBoost", "addedhealthmechanics:elixir:13");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:15', ':AHM_elixir_damageBoost', 'upgStrengthBase', 'minecraft:blaze_powder', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:14', ':AHM_elixir_damageBoost', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:13', ':AHM_potion_damageBoost', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_moveSpeed", "addedhealthmechanics:elixir:18");
+mod.addToOreDictionary("AHM_elixir_moveSpeed", "addedhealthmechanics:elixir:17");
+mod.addToOreDictionary("AHM_elixir_moveSpeed", "addedhealthmechanics:elixir:16");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:18', ':AHM_elixir_moveSpeed', 'upgStrengthBase', 'minecraft:blaze_powder', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:17', ':AHM_elixir_moveSpeed', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:16', ':AHM_potion_moveSpeed', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_fireResistance", "addedhealthmechanics:elixir:20");
+mod.addToOreDictionary("AHM_elixir_fireResistance", "addedhealthmechanics:elixir:19");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:20', ':AHM_elixir_fireResistance', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:19', ':AHM_potion_fireResistance', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_resistance", "addedhealthmechanics:elixir:23");
+mod.addToOreDictionary("AHM_elixir_resistance", "addedhealthmechanics:elixir:22");
+mod.addToOreDictionary("AHM_elixir_resistance", "addedhealthmechanics:elixir:21");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:23', ':AHM_elixir_resistance', 'upgStrengthBase', 'minecraft:blaze_powder', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:22', ':AHM_elixir_resistance', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:21', ':AHM_potion_resistance', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addToOreDictionary("AHM_elixir_waterBreathing", "addedhealthmechanics:elixir:25");
+mod.addToOreDictionary("AHM_elixir_waterBreathing", "addedhealthmechanics:elixir:24");
+
+mod.addShapelessRecipe('addedhealthmechanics:elixir:25', ':AHM_elixir_waterBreathing', 'upgLengthBase', 'ThermalFoundation:material:1025', 'ThermalFoundation:material:4');
+mod.addShapelessRecipe('addedhealthmechanics:elixir:24', ':AHM_potion_waterBreathing', 'upgTierBase', 'ThermalFoundation:material:44');
+
+mod.addShapelessRecipe('addedhealthmechanics:tincture:31', 'addedhealthmechanics:ingredient:7', 'herbalPaste', ':listAllwater', 'harvestcraft:mixingbowlItem');
+mod.addShapelessRecipe('addedhealthmechanics:potion:31', 'addedhealthmechanics:tincture:31', 'upgStrengthBase', 'harvestcraft:chilipepperItem', 'harvestcraft:spiceleafItem');
