581ecd7fe82d532e9eba97bccea128956cb50d3c
pseudobyte <pseudobyte@gmail.com>
2015-03-20 22:39:00 -0600

    made test item initialize cooldown (damn it, CS2) and made bandages and poultices add and respect the cooldown

--- scripts/bandage_use.js
+++ scripts/bandage_use.js
@@ -1,8 +1,22 @@
 if (player.isSneaking()) {
-  if (player.getMaxHealth() == player.getHealth()) {
-    itemstack.setStackSize(itemstack.getStackSize() - 1);
-    player.setHealth(Math.min(player.getHealth() + 3, player.getMaxHealth()));
+  if (player.getMaxHealth() > player.getHealth()) {
+    var nextUse = player.getNbt().getInt("AHMcooldown");
+    player.sendMessage(nextUse);
+    var curTime = world.getTime();
+    player.sendMessage(curTime);
+    if (nextUse > curTime) {
+      var interval = nextUse - curTime;
+      if (interval < 70) {
+        player.sendMessage("Your wounds have been treated recently, wait a few seconds.");
+      } else {
+        player.sendMessage("Your wounds have been treated recently, wait a while.");        
+      }
+    } else {
+      itemstack.setStackSize(itemstack.getStackSize() - 1);
+      player.setHealth(Math.min(player.getHealth() + 3, player.getMaxHealth()));
+      player.getNbt().setInt("AHMcooldown", curTime + 400);
+    }
   } else {
-    player.sendMessage('You\'re in good shape, no need to patch up.');
+    player.sendMessage("You're in good shape, no need to patch up.");
   }
 }
