fd22943a68a972eed9001fd1362af214feb9b8bf
Madcat1030 <madcat1030@gmail.com>
2015-03-21 20:40:29 -0400

    Added poultice bag filling recipes.

--- scripts/medicine.js
+++ scripts/medicine.js
@@ -49,5 +49,67 @@ mod.addRecipe("medPoultice", 3, 2, "minecraft:air", "medLeaf", "minecraft:air", 
 mod.addShapelessRecipe("medBandage 3", "harvestcraft:wovencottonItem", ":slimeball", "medGreen", "medGreen", "medBlue", "medBlue", "medMagenta", "medMagenta");
 
 //poultice bag recipes
-mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:8", "addedhealthmechanics:medbag_basic:8")
-mod.addShapelessRecipe("medPoultice", "addedhealthmechanics:medbag_basic-")
+//Empty = empty
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:8", "addedhealthmechanics:medbag_basic:8");
+//Bag = poultice + 1 dmg
+mod.addShapelessRecipe("medPoultice", "addedhealthmechanics:medbag_basic-");
+
+//Empty + 8 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:8", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+//kit|poult|result
+//1 + 7 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:7", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+//2 + 6 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:6", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+//3 + 5 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:5", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+//4 + 4 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:4", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+//5 + 3 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:3", "medPoultice", "medPoultice", "medPoultice");
+//6 + 2 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:2", "medPoultice", "medPoultice");
+//7 + 1 = full
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:0", "addedhealthmechanics:medbag_basic:1", "medPoultice");
+
+//ALL + 1 = ALL+1
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:1", "addedhealthmechanics:medbag_basic:2", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:2", "addedhealthmechanics:medbag_basic:3", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:3", "addedhealthmechanics:medbag_basic:4", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:4", "addedhealthmechanics:medbag_basic:5", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:5", "addedhealthmechanics:medbag_basic:6", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:6", "addedhealthmechanics:medbag_basic:7", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:7", "addedhealthmechanics:medbag_basic:8", "medPoultice");
+
+//ALL + 2 = ALL+2
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:1", "addedhealthmechanics:medbag_basic:3", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:2", "addedhealthmechanics:medbag_basic:4", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:3", "addedhealthmechanics:medbag_basic:5", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:4", "addedhealthmechanics:medbag_basic:6", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:5", "addedhealthmechanics:medbag_basic:7", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:6", "addedhealthmechanics:medbag_basic:8", "medPoultice", "medPoultice");
+
+//ALL + 3 = ALL+3
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:1", "addedhealthmechanics:medbag_basic:4", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:2", "addedhealthmechanics:medbag_basic:5", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:3", "addedhealthmechanics:medbag_basic:6", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:4", "addedhealthmechanics:medbag_basic:7", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:5", "addedhealthmechanics:medbag_basic:8", "medPoultice", "medPoultice", "medPoultice");
+
+//ALL + 4 = ALL+4
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:1", "addedhealthmechanics:medbag_basic:5", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:2", "addedhealthmechanics:medbag_basic:6", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:3", "addedhealthmechanics:medbag_basic:7", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:4", "addedhealthmechanics:medbag_basic:8", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+
+//ALL + 5 = ALL+3
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:1", "addedhealthmechanics:medbag_basic:6", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:2", "addedhealthmechanics:medbag_basic:7", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:3", "addedhealthmechanics:medbag_basic:8", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+
+//ALL + 6 = ALL+2
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:1", "addedhealthmechanics:medbag_basic:7", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:2", "addedhealthmechanics:medbag_basic:8", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
+
+//Empty + 7 = 7
+mod.addShapelessRecipe("addedhealthmechanics:medbag_basic:1", "addedhealthmechanics:medbag_basic:8", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice", "medPoultice");
