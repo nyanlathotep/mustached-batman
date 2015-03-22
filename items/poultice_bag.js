9bc1b05b885a1cf1a79759ae48b8bb197f7aba34
pseudobyte <pseudobyte@gmail.com>
2015-03-21 18:39:25 -0600

    added med bag script, file, and texture

---
+++ items/poultice_bag.js
@@ -0,0 +1,7 @@
+name = "medbag_basic";
+creativeTab = "Medicine";
+maxStack = 1;
+maxDamage = 8;
+addToCreative[0] = true;
+onRightClick[0] = "mod.loadScript('poultice_bag_use.js');";
+textureFile[0] = "meds_bag";
--- scripts/config.js
+++ scripts/config.js
@@ -1,4 +1,11 @@
 config.addIntProperty("poulticeCooldown", "medicine", 200, "cooldown of a poultice in ticks")
 config.addIntProperty("poulticeRecovery", "medicine", 1, "healing of a poultice in half-hearts")
+
 config.addIntProperty("bandageCooldown", "medicine", 400, "cooldown of a bandage in ticks")
 config.addIntProperty("bandageRecovery", "medicine", 3, "healing of a bandage in half-hearts")
+
+config.addIntProperty("poulticeBagCooldown", "medicine", 150, "cooldown of a poultice bag in ticks")
+config.addIntProperty("poulticeBagRecovery", "medicine", 2, "healing of a poultice bag in half-hearts")
+
+config.addIntProperty("bandageBagCooldown", "medicine", 300, "cooldown of a bandage bag in ticks")
+config.addIntProperty("bandageBagRecovery", "medicine", 5, "healing of a bandage bag in half-hearts")
