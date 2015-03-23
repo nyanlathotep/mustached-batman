c3924a28afbfa2b7416d46b8928bc8a61fa97fc1
pseudobyte <pseudobyte@gmail.com>
2015-03-23 02:14:45 -0600

    added charged regen hearts and regen heart charge script

--- items/consumable.js
+++ items/consumable.js
@@ -27,6 +27,21 @@ textureFile[4] = "regen_heart";
 information[4] = "A thing.";
 onRightClick[4] = "mod.loadScript('regen_heart_charge.js');";
 
+addToCreative[5] = true;
+textureFile[5] = "regen_heart_gold";
+information[5] = "A thing.";
+onRightClick[5] = "mod.loadScript('regen_heart_use.js');";
+
+addToCreative[6] = true;
+textureFile[6] = "regen_heart_full";
+information[6] = "A thing.";
+onRightClick[6] = "mod.loadScript('regen_heart_use.js');";
+
+addToCreative[7] = true;
+textureFile[7] = "regen_heart_ender";
+information[7] = "A thing.";
+onRightClick[7] = "mod.loadScript('regen_heart_use.js');";
+
 addToCreative[31] = true;
 textureFile[31] = "potion_init";
 onRightClick[31] = "player.getNbt().setInt('PlayerPersisted/AHMcooldown', 0); player.sendMessage('Medicine initialized.'); itemstack.setStackSize(0);";
