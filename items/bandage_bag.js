46f3cf5dbab69f9fb4f02f15cf2f43222a1ae4cf
Madcat1030 <madcat1030@gmail.com>
2015-03-23 16:58:54 -0400

    Mixed up tooltip values for Bandage_Bag&Poultice_Bag.

--- items/bandage_bag.js
+++ items/bandage_bag.js
@@ -5,4 +5,4 @@ maxDamage = 8;
 addToCreative[0] = true;
 onUseOnPlayer[0] = "mod.loadScript('bandage_bag_use.js');";
 textureFile[0] = "meds_bag_red";
-information[0] = "Sneak-right click to patch up a partner.|§9Heals target for 1 heart.§r|§eCraft with up to 8 Bandages to refill.|§eCraft alone to retrieve a Bandage.§r"
+information[0] = "Sneak-right click to patch up a partner.|§9Heals target for 2.5 hearts.§r|§eCraft with up to 8 Bandages to refill.|§eCraft alone to retrieve a Bandage.§r"
