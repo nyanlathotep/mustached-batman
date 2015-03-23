46debc7dbb81826850a966c75f40badfb056789c
pseudobyte <pseudobyte@gmail.com>
2015-03-23 17:18:30 -0600

    removed ineffective recipe removals, updated recipe to use damage value 0, added TiC wood rod to stick oredict, updated flint blade to use stick oredict

--- scripts/tweak.js
+++ scripts/tweak.js
@@ -2,8 +2,9 @@
 mod.removeShapelessRecipe("ExtrabiomesXL:extrabiomes.food:0", "minecraft:milk_bucket", "minecraft:sugar", "minecraft:dye:3");
 mod.removeRecipe("minecraft:brewing_stand", 3, 2, "minecraft:air", "minecraft:blaze_rod", "minecraft:air", "minecraft:cobblestone", "minecraft:cobblestone", "minecraft:cobblestone")
 
+// add wooden tool rod to stick oredict
+mod.addToOreDictionary("stickWood", "TConstruct:toolRod:0")
+
 //make carts use slimeball oredict
-mod.removeRecipe("MCP_Wheel:wooden_wheel", 3, 3, "minecraft:stick", "minecraft:stick", "minecraft:stick", "minecraft:stick", "minecraft:slime_ball", "minecraft:stick", "minecraft:stick", "minecraft:stick", "minecraft:stick")
 mod.addRecipe("MCP_Wheel:wooden_wheel", 3, 3, ":stickWood", ":stickWood", ":stickWood", ":stickWood", ":slimeball", ":stickWood", ":stickWood", ":stickWood", ":stickWood")
-mod.removeRecipe("MCP_Cart:wooden_cart", 3, 2, "MCP_Wheel:wooden_wheel", "minecraft:boat", "minecraft:stick", "MCP_Wheel:wooden_wheel", "minecraft:slime_ball", "minecraft:stick")
-mod.addRecipe("MCP_Cart:wooden_cart", 3, 2, "MCP_Wheel:wooden_wheel", "minecraft:boat", ":stickWood", "MCP_Wheel:wooden_wheel", ":slimeball", ":stickWood")
+mod.addRecipe("MCP_Cart:wooden_cart", 3, 2, "MCP_Wheel:wooden_wheel:0", "minecraft:boat:0", ":stickWood", "MCP_Wheel:wooden_wheel:0", ":slimeball", ":stickWood")
