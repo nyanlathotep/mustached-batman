cd1b3f861d886fbed6d78fd6675fd10988b1b857
Madcat1030 <madcat1030@gmail.com>
2015-03-23 04:25:56 -0400

    Added regen heart recipes

---
+++ scripts/regen.js
@@ -0,0 +1,19 @@
+//Regenerate lost hearts and gain more hearts above 10
+
+//#_#
+//#X#
+//_#_
+//tier 1 heart (.5 up to 5).
+//#=honey
+//X=gold ingot
+mod.addRecipe("addedhealthmechanics:consumable:2", 3, 3, "harvestcraft:honeyItem", "minecraft:air", "harvestcraft:honeyItem", "harvestcraft:honeyItem", "minecraft:gold_ingot", "harvestcraft:honeyItem", "minecraft:air", "harvestcraft:honeyItem", "minecraft:air")
+
+//tier 2 heart (.5 up to 10, minimum is to 5.5)
+//#=diamond
+//X=redstone
+mod.addRecipe("addedhealthmechanics:consumable:2", 3, 3, "minecraft:redstone", "minecraft:air", "minecraft:redstone", "minecraft:redstone", "minecraft:diamond", "minecraft:redstone", "minecraft:air", "minecraft:redstone", "minecraft:air")
+
+//tier 3 heart (1 up to 20, minimum 11.)
+//#=diamond block
+//X=enderium
+mod.addRecipe("addedhealthmechanics:consumable:2", 3, 3, "ThermalFoundation:material:76", "minecraft:air", "ThermalFoundation:material:76", "ThermalFoundation:material:76", "minecraft:diamond_block", "ThermalFoundation:material:76", "minecraft:air", "ThermalFoundation:material:76", "minecraft:air")
--- scripts/tweak.js
+++ scripts/tweak.js
@@ -1,3 +1,30 @@
 //Extra compatability tweaking
 mod.removeShapelessRecipe("ExtrabiomesXL:extrabiomes.food:0", "minecraft:milk_bucket", "minecraft:sugar", "minecraft:dye:3");
-mod.removeRecipe("minecraft:brewing_stand", 3, 2, "minecraft:air", "minecraft:blaze_rod", "minecraft:air", "minecraft:cobblestone", "minecraft:cobblestone", "minecraft:cobblestone")
+mod.removeRecipe("minecraft:brewing_stand", 3, 2, "minecraft:air", "minecraft:blaze_rod", "minecraft:air", "minecraft:cobblestone", "minecraft:cobblestone", "minecraft:cobblestone");
+mod.removeShapelessRecipe("Metallurgy:nether.dust:3", "Metallurgy:nether.dust:3", "minecraft:glowstone_dust");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustCoal");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustObsidian");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustSulfur");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustSaltpeter");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustSignalum");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustLumium");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustPyrotheum");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustCryotheum");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustBlizz");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustGraphite");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustPotash");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustPlastic");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustMidasium");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustPhosphorus");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustMagnesium");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustBitumen");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustSalt");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustWheat");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustFluix");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustEnder");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustEnderPearl");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustEnderium");
+mod.removeShapelessRecipe("ThermalFoundation:material:1", "Metallurgy:nether.dust:3", ":dustWood");
+
+
+
