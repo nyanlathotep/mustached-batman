154045b0ad639ba16e88a73fb83aa4a6fdb04cc8
pseudobyte <pseudobyte@gmail.com>
2015-03-24 01:04:26 -0600

    removed cart tweaks

--- scripts/tweak.js
+++ scripts/tweak.js
@@ -4,7 +4,3 @@ mod.removeRecipe("minecraft:brewing_stand", 3, 2, "minecraft:air", "minecraft:bl
 
 // add wooden tool rod to stick oredict
 mod.addToOreDictionary("stickWood", "TConstruct:toolRod:0")
-
-//make carts use slimeball oredict
-mod.addRecipe("MCP_Wheel:wooden_wheel", 3, 3, ":stickWood", ":stickWood", ":stickWood", ":stickWood", ":slimeball", ":stickWood", ":stickWood", ":stickWood", ":stickWood")
-mod.addRecipe("MCP_Cart:wooden_cart", 3, 2, "MCP_Wheel:wooden_wheel:0", "minecraft:boat:0", ":stickWood", "MCP_Wheel:wooden_wheel:0", ":slimeball", ":stickWood")
