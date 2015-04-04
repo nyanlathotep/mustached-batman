00ed304bd333afef8bd16bd9bac62522fc0c635f
pseudobyte <pseudobyte@gmail.com>
2015-04-03 18:04:01 -0600

    removed ineffective recipe tweak

--- scripts/tweak.js
+++ scripts/tweak.js
@@ -1,5 +1,4 @@
 //Extra compatability tweaking
-mod.removeShapelessRecipe("ExtrabiomesXL:extrabiomes.food:0", "minecraft:milk_bucket", "minecraft:sugar", "minecraft:dye:3");
 mod.removeRecipe("minecraft:brewing_stand", 3, 2, "minecraft:air", "minecraft:blaze_rod", "minecraft:air", "minecraft:cobblestone", "minecraft:cobblestone", "minecraft:cobblestone")
 
 // add wooden tool rod to stick oredict
